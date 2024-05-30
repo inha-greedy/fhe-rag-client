4d87d6a72fb2313fadae571ef6bc3fceb4420216
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2024-05-30 16:40:01 +0900

    feat(storage): add key fingerprint

--- client/routes/key.py
+++ client/routes/key.py
@@ -2,12 +2,17 @@ import os
 from fastapi import APIRouter, UploadFile, Form
 from fastapi.responses import FileResponse
 
-from ..services.storage import save_zipfile, save_public_key, save_all_key
+from ..services.storage import (
+    save_zipfile,
+    save_public_key,
+    save_all_key,
+    get_image_path,
+    get_all_key_path,
+)
 from ..services.key import (
     load_he_from_key,
     send_public_key_to_server,
     setup_ckks_context,
-    get_all_key_path,
 )
 
 key_router = APIRouter()
@@ -18,17 +23,18 @@ async def set_encryption_key(file: UploadFile = None, step: int = Form(...)):
     if step == 1:
         if file is not None:
             contents = await file.read()
-            key_name = file.filename
             save_zipfile(contents=contents)
             he = load_he_from_key()
             save_all_key(he=he)
             save_public_key(he=he)
-            return {"key_name": key_name}
 
         else:
             he = setup_ckks_context()
             save_all_key(he=he)
             save_public_key(he=he)
+
+        image_path = get_image_path()
+        return FileResponse(image_path, media_type="image/png")
 
     elif step == 2:
         # 2. public file -> storage-server
--- client/services/storage.py
+++ client/services/storage.py
@@ -1,5 +1,6 @@
 import os
 import zipfile
+from PIL import Image
 
 from Pyfhel import Pyfhel
 
@@ -8,6 +9,7 @@ from .session import get_user_id
 
 def save_public_key(he: Pyfhel) -> None:
     zip_file_path = get_public_key_path()
+    image_path = get_image_path()
 
     with zipfile.ZipFile(zip_file_path, "w") as zipf:
         zipf.writestr("context.bytes", he.to_bytes_context())
@@ -15,10 +17,89 @@ def save_public_key(he: Pyfhel) -> None:
         zipf.writestr("relin_key.bytes", he.to_bytes_relin_key())
         zipf.writestr("rotate_key.bytes", he.to_bytes_rotate_key())
 
+    input_data = he.to_bytes_public_key()[1500:1506]
+
+    # 고유 프랙탈 이미지 생성
+    image = create_fractal_image(input_data)
+    image.save(image_path)
+
+
+def create_fractal_image(input_data, width: int = 800, height: int = 800):
+    """
+    Create a fractal image based on input data and save it to a file.
+
+    Parameters
+    ----------
+
+    input_data : bytes or int or str
+        Data used to generate fractal parameters.
+        Can be bytes, an integer, or a bit-string (str).
+    width : int
+        Width of the image in pixels (default is 800).
+    height : int
+        Height of the image in pixels (default is 800).
+
+
+    Returns
+    -------
+    image : :py:class:`~PIL.Image.Image`
+        A fractal image
+    """
+
+    if isinstance(input_data, bytes):  # bytes
+        input_data = input_data[:48]
+        bits = "".join(format(byte, "08b") for byte in input_data)
+
+        if len(bits) < 48:
+            raise ValueError("length of bits must be up to 48.")
+
+    elif isinstance(input_data, int):  # bit
+        bits = format(input_data, "048b")
+
+    elif isinstance(input_data, str):  # bit-string
+        bits = input_data[:48]
+
+        if len(bits) < 48:
+            raise ValueError("length of bits must be up to 48.")
+
+    else:
+        raise TypeError("input_data must be in [ bytes, int, str(bit-string) ].")
+
+    # set RGB color
+    r = int(bits[:8], 2) % 256
+    g = int(bits[8:16], 2) % 256
+    b = int(bits[16:24], 2) % 256
+
+    # set fractal parameters
+    max_iterations = int(bits[24:32], 2) % 100 + 50
+    bailout = int(bits[32:40], 2) / 256.0 + 2.0
+    power = int(bits[40:48], 2) / 16.0 + 1.0
+
+    # set image size
+    fractal_image = Image.new("RGB", (width, height))
+
+    # generate fractal art
+    for x in range(width):
+        for y in range(height):
+            cx = (x - width / 2) / (width / 4.0)
+            cy = (y - height / 2) / (height / 4.0)
+
+            c = complex(cx, cy)
+            z = 0j
+
+            for i in range(max_iterations):
+                z = z**power + c
+                if abs(z) > bailout:
+                    break
+
+            color = (r * i % 256, g * i % 256, b * i % 256)
+            fractal_image.putpixel((x, y), color)
+
+    return fractal_image
+
 
 def save_all_key(he: Pyfhel) -> None:
     zip_file_path = get_all_key_path()
-    print(f"{zip_file_path=}")
 
     with zipfile.ZipFile(zip_file_path, "w") as zipf:
         zipf.writestr("context.bytes", he.to_bytes_context())
@@ -26,6 +107,8 @@ def save_all_key(he: Pyfhel) -> None:
         zipf.writestr("relin_key.bytes", he.to_bytes_relin_key())
         zipf.writestr("rotate_key.bytes", he.to_bytes_rotate_key())
         zipf.writestr("sec.key.bytes", he.to_bytes_secret_key())
+
+    print(f"key saved: {zip_file_path}")
 
 
 def save_zipfile(contents: bytes) -> None:
@@ -73,3 +156,8 @@ def get_public_key_path() -> str:
 def get_all_key_path() -> str:
     storage_path = _get_storage_path()
     return os.path.join(storage_path, "key.zip")
+
+
+def get_image_path() -> str:
+    storage_path = _get_storage_path()
+    return os.path.join(storage_path, "image.png")
